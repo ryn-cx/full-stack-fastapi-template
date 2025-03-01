e4c776437e6a37c5740fcb808456eb3b0a1a2bfd
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-03-01 12:21:51 -0800

    Refactored routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter
 
-from app.api.routes import items, login, private, users, utils
+from app.api.routes import login, private, utils
 from app.core.config import settings
+from app.item import router
+from app.user import users
 
 api_router = APIRouter()
 api_router.include_router(login.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
-api_router.include_router(items.router)
+api_router.include_router(router.router)
 
 
 if settings.ENVIRONMENT == "local":
