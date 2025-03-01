8ce9dceef5b377d4de00762d12c78684547522a8
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-03-01 12:28:04 -0800

    Renamed and moved route files

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,16 +1,17 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, private, utils
+from app.api.routes import utils
 from app.core.config import settings
 from app.items import router
+from app.login import routes
 from app.users import users
 
 api_router = APIRouter()
-api_router.include_router(login.router)
+api_router.include_router(routes.router)
 api_router.include_router(users.router)
 api_router.include_router(utils.router)
 api_router.include_router(router.router)
 
 
 if settings.ENVIRONMENT == "local":
-    api_router.include_router(private.router)
+    api_router.include_router(routes.router)
