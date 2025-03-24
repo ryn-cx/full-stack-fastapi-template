84c7f7278bc5c322b5c707af7fd61aa5280609a3
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-03-23 17:24:32 -0700

    Added automatic model loading.
    The previous model loading method was for SQLAlchemy and did not actually work with SQLModel anyways.

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,28 @@
-from app.items.models import SQLModel as ItemSQLModel
-from app.users.models import SQLModel as UserSQLModel
+from importlib import import_module
+from pathlib import Path
 
-target_metadata = [UserSQLModel.metadata, ItemSQLModel.metadata]
+from sqlmodel import SQLModel
+
+
+def automatically_load_models() -> None:
+    """Automatically load all of the models from app/*/models.py"""
+    app_folder = Path(__file__).parent
+
+    for model_files in app_folder.glob("*/models.py"):
+        module_name = model_files.parent.name
+        import_module(f"app.{module_name}.models")
+
+
+def manually_load_models() -> None:
+    """Manually load all of the models.
+
+    This function is left as an example of how to manually load models if they do not
+    fit the folder structure supported by automatically_load_models."""
+
+    # Example of how to import models manually
+    # from app.items.models import Item
+    # from app.users.models import User
+
+
+automatically_load_models()
+target_metadata = SQLModel.metadata
