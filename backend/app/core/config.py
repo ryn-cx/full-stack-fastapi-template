6eb1f89ae4fdf4bb191e7392f2aec1b93c2dd6f7
ryn-cx <84663588+ryn-cx@users.noreply.github.com>
2025-04-19 22:17:12 -0700

    Run tests on a separate test database.
    This makes it easier to manually test things because the database does not get cleared every time a test is ran and no junk data is left behind.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -56,6 +56,7 @@ class Settings(BaseSettings):
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
+    POSTGRES_TEST_DB: str = ""
 
     @computed_field  # type: ignore[prop-decorator]
     @property
@@ -67,6 +68,18 @@ class Settings(BaseSettings):
             host=self.POSTGRES_SERVER,
             port=self.POSTGRES_PORT,
             path=self.POSTGRES_DB,
+        )
+        
+    @computed_field  # type: ignore[prop-decorator]
+    @property
+    def SQLALCHEMY_TEST_DATABASE_URI(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_SERVER,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_TEST_DB,
         )
 
     SMTP_TLS: bool = True
--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -2,33 +2,42 @@ from collections.abc import Generator
 
 import pytest
 from fastapi.testclient import TestClient
-from sqlmodel import Session, delete
+from sqlalchemy import Engine, create_engine
+from sqlmodel import Session, SQLModel, create_engine
 
 from app.core.config import settings
-from app.core.db import engine, init_db
-from app.items.models import Item
+from app.core.db import init_db
+from app.dependencies import get_db
 from app.main import app
 from app.tests.utils.user import authentication_token_from_email
 from app.tests.utils.utils import get_superuser_token_headers
-from app.users.models import User
 
 
 @pytest.fixture(scope="session", autouse=True)
-def db() -> Generator[Session, None, None]:
-    with Session(engine) as session:
+def test_engine() -> Generator[Engine, None, None]:
+    test_engine = create_engine(str(settings.SQLALCHEMY_TEST_DATABASE_URI))
+    SQLModel.metadata.drop_all(bind=test_engine)
+    SQLModel.metadata.create_all(test_engine)
+    yield test_engine
+    SQLModel.metadata.drop_all(bind=test_engine)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def db(test_engine: Engine) -> Generator[Session, None, None]:
+    with Session(test_engine) as session:
         init_db(session)
         yield session
-        statement = delete(Item)
-        session.execute(statement)
-        statement = delete(User)
-        session.execute(statement)
-        session.commit()
 
 
 @pytest.fixture(scope="module")
-def client() -> Generator[TestClient, None, None]:
+def client(db: Session) -> Generator[TestClient, None, None]:
+    def get_db_override() -> Session:
+        return db
+
+    app.dependency_overrides[get_db] = get_db_override
     with TestClient(app) as c:
         yield c
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture(scope="module")
